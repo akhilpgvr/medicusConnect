8855ffe4ca50807d5524d49eda0601cfc4e1c57f
akhilpgvr <akhilpgvr@gmail.com>
2024-10-01 19:03:36 +0530

    Added routing to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { Profiler } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Appointment from './pages/Appointment'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%'>
+      <Routes>
+        <Route path='/' element={<Home />} /> {/*Home page*/}
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointment/:' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Appointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Appointment = () => {
+  return (
+    <div>
+      Appointment page
+    </div>
+  )
+}
+
+export default Appointment
