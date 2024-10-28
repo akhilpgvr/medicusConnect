3b19db916f8c1ab8003cfbff8a5c6e27b9404223
akhilpgvr <akhilpgvr@gmail.com>
2024-10-28 20:25:47 +0530

    added speciality section

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Doctors = () => {
+
+  const {speciality} = useParams()
+
+  console.log(speciality);
+
+  // const{doctors} = useContext(AppContext) 
+  // const[filterDoc, setFilterDoc] = useState([])
+
   return (
+
     <div>
-      
+      <div>
+        <p>General Physician</p>
+        <p>Gynecologist</p>
+        <p>Dermatologist</p>
+        <p>Pediatrician</p>
+        <p>Neurologist</p>
+        <p>Gastroenterologist</p>
+      </div>
+
+      <div>
+        {
+          // filterDoc.map()
+        }
+      </div>
     </div>
   )
 }
